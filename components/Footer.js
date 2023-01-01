387f1691fd5c65d23c64d0f07b0b7a7cf8e1571f
Jian Wang <jianwang2010@gmail.com>
2023-01-01 01:52:30 -0500

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Copyright 2023 Jonathan Inc.</div>
+  )
+}
+
+export default Footer
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Excepteur culpa tempor consectetur excepteur. Nisi magna enim amet officia ea irure esse. Ipsum ut incididunt veniam id est culpa esse.</p>
+        <p>Nisi dolor culpa mollit sit magna enim tempor adipisicing. Pariatur nostrud incididunt aliquip est fugiat nisi. Commodo excepteur tempor mollit quis pariatur nisi aute incididunt velit mollit aliquip duis id elit. Consequat elit et adipisicing aliquip ut irure ea id voluptate reprehenderit exercitation mollit.</p>
+    </div>
+  )
+}
+
+export default About
