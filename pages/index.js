c8810d72781915699e301badf4a33b149f62adc6
Jian Wang <jianwang2010@gmail.com>
2023-01-01 19:01:15 -0500

    Updated home page information

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="keywords" content="Ranking Marathon Finishing Time" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
+        <h1 className={styles.title}>Marathan Finish Time Ranking</h1>
         <p className={styles.text}>Dolore esse minim irure ex in fugiat pariatur laborum sunt aliquip. Dolore laboris exercitation incididunt reprehenderit anim cupidatat est excepteur exercitation voluptate. Labore eiusmod voluptate reprehenderit tempor exercitation. Anim amet veniam fugiat ad consectetur labore aliqua fugiat.</p>
         <p className={styles.text}>Occaecat deserunt eu id proident consectetur deserunt nostrud fugiat quis voluptate labore cillum. Commodo elit proident mollit sint culpa proident fugiat excepteur nisi nulla anim. Nostrud incididunt nulla reprehenderit officia amet sit elit cillum aliquip amet nostrud mollit. Nisi do enim tempor Lorem.</p>
         
